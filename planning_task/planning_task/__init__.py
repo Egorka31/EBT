ce326030d8c204fe0e5125b8c7adf2626445423e
Egorka31 <ego-koto@yandex.ru>
2019-03-25 23:59:04 +0300

    Delete __init__.py

--- planning_task/planning_task/__init__.py
+++
@@ -1,7 +0,0 @@
-from .object import Object
-from .predicate import Predicate
-from .action import Action
-from .state import State
-from .task import Task
-
-__all__ = ['Object', 'Predicate', 'Action', 'State', 'Task']
