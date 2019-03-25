78265e4894fe00462aad897e6cc53b262f7eb656
Egorka31 <ego-koto@yandex.ru>
2019-03-25 23:59:38 +0300

    Delete predicate.py

--- planning_task/planning_task/predicate.py
+++
@@ -1,8 +0,0 @@
-from .base import Parameterizable
-
-
-class Predicate(Parameterizable):
-
-    def __new__(cls, name, parameters):
-        self = super(__class__, cls).__new__(cls, name, parameters)
-        return self
